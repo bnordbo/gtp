e08f2f030946a135fb8d9a83fe161da98624ad9c
Bjørn Nordbø <bn@strangedays.no>
2017-01-02 18:01:32 +0100

    Parse the optional header parts

--- src/header.rs
+++ src/header.rs
@@ -1,10 +1,13 @@
 use info::InfoElement;
-use parser::{Parser, ParseResult};
+use parser::{Parser, ParseError, ParseResult};
 use std::collections::hash_set::{HashSet};
 
 // TODO: Add message type too.
+// TODO: This started out as a direct translation of the protocol type,
+//       but some of the fields are likely only required for parsing,
+//       e.g. flags and length and can be removed.
 #[derive(Debug)]
-pub struct Gtp {
+pub struct Gtp<'a> {
     pub version: Version,
     pub protocol: Protocol,
     pub flags: Flags,
@@ -12,24 +15,20 @@ pub struct Gtp {
     pub teid: TunnelEid,
     pub seq_num: Option<SequenceNumber>,
     pub npdu_num: Option<NPduNumber>,
-    pub next_ext_type: Option<NextExtHeaderType>,
-    // TODO: Implement support for extension headers.
+    pub ext_hdrs: Vec<ExtensionHeader<'a>>,
 }
 
-impl Gtp {
-    pub fn parse(p: &mut Parser) -> ParseResult<Gtp> {
+impl<'a> Gtp<'a> {
+    pub fn parse(p: &mut Parser<'a>) -> ParseResult<Gtp<'a>> {
         let top   = p.parse_u8()?;
         let ver   = Version::parse(top)?;
         let proto = Protocol::parse(top)?;
         let flags = Flags::parse(top)?;
         let len   = Length::parse(p)?;
         let teid  = TunnelEid::parse(p)?;
-        let seq_num = if flags.contains(&Flag::SequenceNumber) {
-            SequenceNumber::parse(p).map(Some)?
-        } else {
-            None
-        };
+        let seq_num = flags.parse_seq_num(p)?;
         let npdu_num = flags.parse_npdu(p)?;
+        let ext_hdrs = flags.parse_ext_hdrs(p)?;
         Ok(Gtp {
             version: ver,
             protocol: proto,
@@ -38,7 +37,7 @@ impl Gtp {
             teid: teid,
             seq_num: seq_num,
             npdu_num: npdu_num,
-            next_ext_type: None
+            ext_hdrs: ext_hdrs
         })
     }
 }
@@ -79,11 +78,33 @@ impl Flags {
         Ok(Flags(res))
     }
 
-    pub fn parse_npdu(&self, p: &mut Parser) -> ParseResult<Option<NPduNumber>> {
+    fn parse_seq_num(&self, p: &mut Parser)
+                     -> ParseResult<Option<SequenceNumber>> {
+        if self.contains(&Flag::SequenceNumber) {
+            SequenceNumber::parse(p).map(Some)
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn parse_npdu(&self, p: &mut Parser)
+                      -> ParseResult<Option<NPduNumber>> {
         if self.contains(&Flag::NPduNumber) {
             NPduNumber::parse(p).map(Some)
         } else {
             Ok(None)
+        }
+    }
+
+    pub fn parse_ext_hdrs<'a>(&self, p: &mut Parser<'a>)
+                              -> ParseResult<Vec<ExtensionHeader<'a>>> {
+        if self.contains(&Flag::ExtensionHeader) {
+            let kind = ExtHeaderType::parse(p)?;
+            let mut res = Vec::with_capacity(2);
+            ExtensionHeader::parse(p, kind, &mut res)?;
+            Ok(res)
+        } else {
+            Ok(Vec::with_capacity(0))
         }
     }
 
@@ -155,22 +176,60 @@ impl NPduNumber {
     }
 }
 
-#[derive(Debug)]
-pub enum NextExtHeaderType {
-    EndReached,               // 00000000
-    MbmsSupport,              // 00000001, Control
-    MsInfoChangeReporting,    // 00000010, Control
-    UdpPort,                  // 01000000, User
-    PdcpPdu,                  // 11000000
-    SuspendRequest,           // 11000001, Control
-    SuspendResponse,          // 11000010, Control
+#[derive(Debug, Eq, PartialEq)]
+pub enum ExtHeaderType {
+    EndReached,
+    MbmsSupport,
+    MsInfoChangeReporting,
+    UdpPort,
+    PdcpPdu,
+    SuspendRequest,
+    SuspendResponse,
+}
+
+impl ExtHeaderType {
+    pub fn parse(p: &mut Parser) -> ParseResult<Self> {
+        let t = p.parse_u8()?;
+        match t {
+            0b00000000 => Ok(ExtHeaderType::EndReached),
+            0b00000001 => Ok(ExtHeaderType::MbmsSupport),
+            0b00000010 => Ok(ExtHeaderType::MsInfoChangeReporting),
+            0b01000000 => Ok(ExtHeaderType::UdpPort),
+            0b11000000 => Ok(ExtHeaderType::PdcpPdu),
+            0b11000001 => Ok(ExtHeaderType::SuspendRequest),
+            0b11000010 => Ok(ExtHeaderType::SuspendResponse),
+            _          => Err(ParseError::UnsupportedExtensionHeader(t))
+        }
+    }
 }
 
 #[derive(Debug)]
-pub struct NextExtensionHeader {
-    pub length: u8,
-    pub content: Vec<u8>,
-    pub next_ext_type: NextExtHeaderType
+pub struct ExtensionHeader<'a> {
+    pub kind: ExtHeaderType,
+    pub content: &'a [u8],
+}
+
+impl<'a> ExtensionHeader<'a> {
+    fn new(kind: ExtHeaderType, content: &'a [u8]) -> Self {
+        ExtensionHeader {
+            kind: kind,
+            content: content
+        }
+    }
+
+    pub fn parse(p: &mut Parser<'a>,
+                 kind: ExtHeaderType,
+                 res: &mut Vec<ExtensionHeader<'a>>)
+                 -> ParseResult<()> {
+        let length = p.parse_u8().map(|l| l * 4)?;
+        let content = p.parse(length as usize)?;
+        let next = ExtHeaderType::parse(p)?;
+        res.push(ExtensionHeader::new(kind, content));
+        if next != ExtHeaderType::EndReached {
+            ExtensionHeader::parse(p, next, res)?;
+        }
+        Ok(())
+    }
 }
 
 
